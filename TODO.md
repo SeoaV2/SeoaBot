16d3c206dbea1baeeb98222d6d2a212080216179
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-09-27 19:39:21 +0900

    Settings Command Added

--- TODO.md
+++ TODO.md
@@ -3,21 +3,18 @@
 - [x] >invite, >초대링크, >봇초대링크
 - [x] >botinfo, >봇정보
 - [x] >quiz, >퀴즈
-- [ ] >leader, >leaderboard, >리더보드, >리더, >보드
+- [x] >leader, >leaderboard, >리더보드, >리더, >보드
+- [x] >set, >setting, >설정, >settings 
 - [ ] >m, >music, >뮤직, >음악
 - [ ] >mylist, >재생목록, >마이리스트
 - [ ] >help, >도움, >도움말
-- [ ] >set, >setting, >설정, >settings 
 - [ ] >blacklist, >black, >블랙리스트, >블랙
-- [ ] >업뎃 >업데이트 >다운그래이드(?)
-- [ ] >상태 >status
 
 # Develop
 - 카테고리 별로 폴더 
 - 라바링크 활용
 - 최대한 새 모듈 사용 (yarn, knex 등..)
 - 속도보다 가독성
-- 이미 짰던 코드는 재사용(가능하면...)
 - 설정파일은 짧게
 - db 는 뭘로? mariadb
 - 새롭게 배워야 겠누(???)
@@ -32,11 +29,7 @@
 | `/events/`   | 이벤트시 실행되는 함수 (로그인 했을때, 메세지 왔을때 등)  |
 | `/utils/`    | 구동을 위한 유틸들 (명령어 카테고리 분류 등) |
 | `/commands/` | 명령어 파일들 |
-| `/locales/` | i18n 번역파일들 |
-| `/web/` | 봇 관련 웹사이트들 |
-
-## Category
-| `/commands/about` | 봇관련 명령어 파일들(ping, invite등) |
+| `/locales/`  | i18n 번역파일들 |
 
 
 [기타](https://github.com/seoaapp/SeoaBot/issues/68)
---
+++ commands/settings.js
@@ -0,0 +1,46 @@
+const { MessageEmbed } = require('discord.js')
+const modifiable = ['locale']
+
+/**
+ * @param {import('../classes/Client')} client
+ * @param {import('discord.js').Message} msg
+*/
+async function fn (client, msg, locale) {
+  const [data] = await client.db.select('*').where('id', msg.author.id).from('userdata')
+  const args = msg.query.args
+
+  if (args.length < 1) {
+    const embed = new MessageEmbed({
+      color: 0xff5ae5,
+      title: locale('settings.title'),
+      description: locale('settings.desc', client.settings.prefix)
+    })
+
+    for (const key of Object.keys(data)) {
+      embed.addField(key, data[key], data[key].length < 20)
+    }
+
+    return msg.channel.send(embed)
+  }
+
+  if (!args[1]) return msg.channel.send(new MessageEmbed({ title: locale('settings.novalue', client.settings.prefix), color: 0xff0000 }))
+  if (!modifiable.includes(args[0])) return msg.channel.send(new MessageEmbed({ title: locale('settings.notmodfiable', args[0]), color: 0xff0000 }))
+
+  if (args[0] === 'locale' && !client.i18n.getLocales().includes(args[1])) {
+    msg.channel.send(new MessageEmbed({
+      color: 0xff0000,
+      title: locale('settings.localenotfound', args[1]),
+      description: locale('settings.locales') + '\n- `' + client.i18n.getLocales().join('`\n- `') + '`'
+    }))
+    return
+  }
+
+  const obj = {}
+  obj[args[0]] = args[1]
+  await client.db.update(obj).where('id', msg.author.id).from('userdata')
+
+  msg.channel.send(new MessageEmbed({ color: 0x00ff00, title: locale('settings.done', args[0], args[1]) }))
+}
+
+module.exports = fn
+module.exports.aliases = ['set', 'settings', '설정', 'settings']
