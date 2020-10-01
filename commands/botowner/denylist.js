61093f1adfb3e989cccc518bfe4e1eab26f05b10
Catry <catry.me@gmail.com>
2020-10-01 23:27:26 +0900

    denylist list command

--- commands/botowner/denylist.js
+++ commands/botowner/denylist.js
@@ -8,10 +8,10 @@ async function fn (client, msg, locale) {
   const id = msg.query.args[1]
   const choice = msg.query.args[0]
   if (choice === "add"){
-    await client.db.increment('denylist', 'deny').where('id', id).from('userdata')
+    await client.db.increment('denylist',  id).where('id', id).from('userdata')
   }
   if (choice === "list"){
-    const users = await client.db.select('id', 'denylist').from('userdata')
+    const users = await client.db.select('id', 'denylist').from('userdata').where('denylist', id)
     msg.channel.send(users)
   }
 }
