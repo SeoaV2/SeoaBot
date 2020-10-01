64ba238025a6f2414f37548774b17bdf9168ad36
Catry <catry.me@gmail.com>
2020-10-01 23:11:20 +0900

    add denylist command

--- commands/botowner/denylist.js
+++ commands/botowner/denylist.js
@@ -1,1 +1,19 @@
-//somethings
+const Knex = require('knex')
+
+/**
+ * @param {import('../../classes/Client')} client
+ * @param {import('discord.js').Message} msg
+*/
+async function fn (client, msg, locale) {
+  const id = msg.query.args[1]
+  const choice = msg.query.args[0]
+  if (choice === "add"){
+    await client.db.increment('denylist', 'deny').where('id', id).from('userdata')
+  }
+  if (choice === "list"){
+    const users = await client.db.select('id', 'denylist').from('userdata')
+    msg.channel.send(users)
+  }
+}
+module.exports = fn
+module.exports.aliases = ['denylist', 'deny']
