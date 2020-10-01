b57892f69464df930e94ebccc894fe3a947c5bd3
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-10-01 22:07:37 +0900

    hot fix

--- commands/music/channel/join.js
+++ commands/music/channel/join.js
@@ -4,6 +4,7 @@
 */
 async function fn (client, msg, locale) {
   if (!msg.guild) return msg.channel.send(locale('music.global.nodm'))
+  if (!msg.member.voice.channel) return msg.channel.send(locale('music.global.nochannel'))
   const player = await client.lavalink.join({ guild: msg.guild.id, channel: msg.member.voice.channel.id, node: 'main' })
   msg.channel.send(locale('music.join'))
   player.on('error', (err) => msg.channel.send(locale('music.global.error', err.error)))
--- commands/music/play/play.js
+++ commands/music/play/play.js
@@ -7,6 +7,7 @@ const { MessageEmbed } = require('discord.js')
 */
 async function fn (client, msg, locale) {
   if (!msg.guild) return msg.channel.send(locale('music.global.nodm'))
+  if (!msg.member.voice.channel) return msg.channel.send(locale('music.global.nochannel'))
   let player = client.lavalink.players.get(msg.guild.id)
   if (!player) player = await client.lavalink.join({ guild: msg.guild.id, channel: msg.member.voice.channel.id, node: 'main' })
   const [data] = (await getSongs(client.lavalink.nodes.get('main'), 'ytsearch:' + msg.query.args.join(' '))).tracks
