d72fe7fca6dffd7ad3f2133922c7aff3ba6eee15
=Catry <catry.me@gmail.com>
2020-10-01 22:30:44 +0900

    fix denylist mylist sql

--- database.sql
+++ database.sql
@@ -7,8 +7,8 @@ create table userdata
 (
 	id varchar(20) not null,
 	locale varchar(5) default 'en-US' not null,
-	denylist varchar(1000) not null,
-	mylist varchar(1000) not null,
+	denylist varchar(20) not null,
+	mylist varchar(200) not null,
 	registedAt TIMESTAMP default CURRENT_TIMESTAMP not null,
 	quizscore int default 0 not null
 );
--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const musicEnd = require('./events/musicEnd')
 client.start()
 client.regist('ready', onReady)
 client.regist('message', onMessage)
+// idk music end loop (Tmfoem?)
 client.on('message', (msg) => {
     let player = client.lavalink.players.get(msg.guild.id)
     if (!player) return
