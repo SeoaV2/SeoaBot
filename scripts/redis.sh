8a8c0c456930f6db5db23ccb084c9e08a8efe0fd
=Catry <catry.me@gmail.com>
2020-10-01 21:17:12 +0900

    make redis-server autoinstall

--- scripts/redis.sh
+++ scripts/redis.sh
@@ -1,1 +1,18 @@
+
+if [[ $(redis-server) ]]
+then
+    echo installed redis-server
+else
+    echo installing redis-server
+    wget http://download.redis.io/redis-stable.tar.gz
+    tar xvzf redis-stable.tar.gz
+    cd redis-stable
+    make
+    sudo cp src/redis-server /usr/local/bin/
+    sudo cp src/redis-cli /usr/local/bin/
+    sudo make install
+    cd ..
+    rm -rf redis-stable
+fi
+
 redis-server
