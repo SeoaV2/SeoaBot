c9c5fe503cce9ceb998566ea64d463b27fdfa117
Catry <catry.me@gmail.com>
2020-10-02 12:36:45 +0900

    Fix stop command

--- commands/music/play/stop.js
+++ commands/music/play/stop.js
@@ -6,12 +6,7 @@ async function fn (client, msg, locale) {
   if (!msg.guild) return msg.channel.send(locale('music.global.nodm'))
   const player = client.lavalink.players.get(msg.guild.id)
   player.stop()
-<<<<<<< HEAD:commands/music/stop.js
-  msg.channel.send('byebye')
-  client.musicdb.del(msg.guild.id)
-=======
   msg.channel.send(locale('music.stop'))
->>>>>>> 3c9a566bfebe6f6647d968997ad538d17c1e452c:commands/music/play/stop.js
   client.lavalink.leave(msg.guild.id)
 }
 
