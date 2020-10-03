dfde3560abb86fce4dafaad6c1a8c55f562d64ed
Park Min Hyeok <pmhstudio.pmh@gmail.com>
2020-10-03 21:45:10 +0900

    Fix volume count

--- commands/music/channel/volume.js
+++ commands/music/channel/volume.js
@@ -8,9 +8,9 @@ async function fn (client, msg, locale) {
 
   if (!player) return msg.channel.send(locale('music.global.notplaying'))
 
-  const vol = msg.query.args[0]
+  const vol = Number(msg.query.args[0])
   if (!vol) return msg.channel.send(locale('music.volume.state', player.state.volume))
-  if (vol <= 0 || vol >= 200) return msg.channel.send(locale('music.volume.toohighorlow', client.settings.prefix))
+  if (isNaN(vol) || vol % 1 !== 0 || vol <= 0 || vol >= 200) return msg.channel.send(locale('music.volume.toohighorlow'))
 
   await player.volume(vol)
   msg.channel.send(locale('music.volume.set', vol))
